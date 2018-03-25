1ed7a447a67b31d1a7a30fb23428a1f1283d725c
Kerri Alpiche <kerrialpiche@live.com>
2018-03-25 19:05:12 +0000

    fixed controller errors

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,20 @@
 class QuestionsController < ApplicationController
 
+  def index
+  end
+
   def new
     @question = Question.new
   end
 
-  # def create
-  #   @question = questions.create(question_params)
-  #   redirect_to questions_path
-  # end
+  def create
+    @question = Question.create#(question_params)
+    redirect_to questions_path
+  end
 
   private
 
-  def question_params
-    params.require(:question, :name)
-  end
+  # def question_params
+  #   params.require(:question, :name)
+  # end
 end
